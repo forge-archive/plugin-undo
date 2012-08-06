63d1c1f8587e7a9a33472fd8fad8c7df08405f82
Jevgeni Zelenkov <jevgeni.zelenkov@gmail.com>
2012-08-07 01:14:46 +0200

    Fixed errors in test cases

--- src/test/java/org/jboss/undo/forge/UndoFacetMultipleOpsTest.java
+++ src/test/java/org/jboss/undo/forge/UndoFacetMultipleOpsTest.java
@@ -54,18 +54,21 @@ public class UndoFacetMultipleOpsTest extends AbstractShellTest
    }
 
    private static final String COMMAND_NAME = "touch";
+   private static final String[] FILENAMES = { "test1.txt", "test2.txt", "test3.txt" };
 
    private static Project myProject = null;
    private static DirectoryResource dir = null;
    private static Iterable<RevCommit> commits = null;
    private static List<String> commitMsgs = null;
    private static FileResource<?> file = null;
+   private static String dirPath = null;
 
    @Before
    public void setUp() throws Exception
    {
       myProject = initializeJavaProject();
       dir = myProject.getProjectRoot();
+      dirPath = dir.getFullyQualifiedName();
       getShell().execute("undo setup");
    }
 
@@ -74,6 +77,7 @@ public class UndoFacetMultipleOpsTest extends AbstractShellTest
    {
       myProject = null;
       dir = null;
+      dirPath = null;
       commits = null;
       commitMsgs = null;
       file = null;
@@ -82,99 +86,64 @@ public class UndoFacetMultipleOpsTest extends AbstractShellTest
    @Test
    public void shouldAddTwoChangesIntoUndoBranch() throws Exception
    {
-      String[] filenames = { "test1.txt", "test2.txt" };
-
-      executeForgeCommand(filenames[0]);
-      executeForgeCommand(filenames[1]);
-      // TODO:
-      // verify test1.txt exists
-      // verify test2.txt exists
-
-      // for (Resource<?> each : dir.listResources())
-      // System.err.println(each.getFullyQualifiedName());
-
-      // File fileOne = new File(dir.getFullyQualifiedName() + filenames[0]);
-      // Assert.assertTrue("first file should also exist", fileOne.exists());
-
+      executeForgeCommand(FILENAMES[0]);
+      verifyFilesExistance(true, false);
+      executeForgeCommand(FILENAMES[1]);
+      verifyFilesExistance(true, true);
+      verifyCommitMsgs();
    }
 
    @Test
    public void shouldUndoTwoLastChanges() throws Exception
    {
-      String[] filenames = { "test1.txt", "test2.txt" };
+      executeForgeCommand(FILENAMES[0]);
+      executeForgeCommand(FILENAMES[1]);
 
-      executeForgeCommand(filenames[0]);
-      executeForgeCommand(filenames[1]);
+      undoRestore(FILENAMES[1]);
+      verifyFilesExistance(true, false);
+      verifyCommitMsgs();
 
-      undoRestore(filenames[1]);
-      // TODO:
-      // verify test1.txt exists
-      // verify test2.txt doesn't exist
-
-      undoRestore(filenames[0]);
-      // TODO:
-      // verify test1.txt doesn't exist
-      // verify test2.txt doesn't exist
+      undoRestore(FILENAMES[0]);
+      verifyFilesExistance(false, false);
+      verifyCommitMsgs();
    }
 
    @Test
    public void shouldBeAbleToAddAddRevertAdd() throws Exception
    {
-      String[] filenames = { "test1.txt", "test2.txt", "test3.txt" };
+      executeForgeCommand(FILENAMES[0]);
+      executeForgeCommand(FILENAMES[1]);
+      undoRestore(FILENAMES[1]);
 
-      executeForgeCommand(filenames[0]);
-      executeForgeCommand(filenames[1]);
+      executeForgeCommand(FILENAMES[2]);
+      verifyFilesExistance(true, false, true);
+      verifyCommitMsgs();
+   }
 
-      undoRestore(filenames[1]);
+   @Test
+   public void shouldBeAbleToAddAddRevertRevertAdd() throws Exception
+   {
+      executeForgeCommand(FILENAMES[0]);
+      executeForgeCommand(FILENAMES[1]);
+      undoRestore(FILENAMES[1]);
+      undoRestore(FILENAMES[0]);
+      verifyFilesExistance(false, false);
+      verifyCommitMsgs();
 
-      executeForgeCommand(filenames[2]);
-      // TODO:
-      // verify test1.txt exists
-      // verify test2.txt doesn't exist
-      // verify test3.txt exists
+      executeForgeCommand(FILENAMES[2]);
+      verifyFilesExistance(false, false, true);
+      verifyCommitMsgs();
    }
 
    // helper methods
-   private static List<String> extractCommitMsgs(final Iterable<RevCommit> collection)
-   {
-      List<String> commitMsgs = new ArrayList<String>();
-
-      Iterator<RevCommit> iter = collection.iterator();
-      while (iter.hasNext())
-      {
-         RevCommit commit = iter.next();
-         commitMsgs.add(commit.getFullMessage());
-      }
-
-      return commitMsgs;
-   }
-
    private void executeForgeCommand(String filename)
             throws Exception
    {
       file = dir.getChild(filename).reify(FileResource.class);
-      Assert.assertFalse("file already exists!", file.exists());
+      Assert.assertFalse("file should not already exist", file.exists());
 
       String command = COMMAND_NAME + " " + filename;
       getShell().execute(command);
-
-      file = dir.getChild(filename).reify(FileResource.class);
-      Assert.assertTrue("file doesn't exist", file.exists());
-
-      // verify commit is added into history branch
-      commits = myProject.getFacet(UndoFacet.class).getStoredCommits();
-      commitMsgs = extractCommitMsgs(commits);
-
-      Assert.assertEquals("wrong number of commits in the history branch",
-               getHistoryBranchSize(), commitMsgs.size());
-
-      if (!commitMsgs.isEmpty())
-      {
-         Assert.assertEquals("commit messages do not match",
-                  UndoFacet.UNDO_STORE_COMMIT_MSG_PREFIX + Strings.enquote(COMMAND_NAME) +
-                           " command",
-                  commitMsgs.get(0));
-      }
    }
 
    private void undoRestore(String revertedFile)
@@ -182,13 +151,68 @@ public class UndoFacetMultipleOpsTest extends AbstractShellTest
       boolean isRestored = myProject.getFacet(UndoFacet.class).undoLastChange();
       Assert.assertTrue("undo failed", isRestored);
 
-      file = dir.getChild(revertedFile).reify(FileResource.class);
+      verifyForgeProjectFileExists();
+   }
 
-      Assert.assertFalse("file should not exist", file.exists());
+   private void verifyForgeProjectFileExists()
+   {
+
+      String parent = dir.getChild("src").getChild("main").getChild("resources").getChild("META-INF")
+               .getFullyQualifiedName();
+      String child = "forge.xml";
+      File forgeXml = new File(parent, child);
+      Assert.assertTrue("forge project file doesn't exist anymore", forgeXml.exists());
+   }
+
+   private void verifyFilesExistance(boolean fileOneShouldExist, boolean fileTwoShouldExist)
+   {
+      File tmpFile = null;
+      if (fileOneShouldExist)
+      {
+         tmpFile = new File(dirPath, FILENAMES[0]);
+         Assert.assertTrue("first file should exist", tmpFile.exists());
+      }
+      else
+      {
+         tmpFile = new File(dirPath, FILENAMES[0]);
+         Assert.assertFalse("first file should not exist", tmpFile.exists());
+      }
+
+      if (fileTwoShouldExist)
+      {
+         tmpFile = new File(dirPath, FILENAMES[1]);
+         Assert.assertTrue("second file should exist", tmpFile.exists());
+      }
+      else
+      {
+         tmpFile = new File(dirPath, FILENAMES[1]);
+         Assert.assertFalse("second file should not exist", tmpFile.exists());
+      }
+   }
+
+   private void verifyFilesExistance(boolean fileOneShouldExist, boolean fileTwoShouldExist,
+            boolean fileThreeShouldExist)
+   {
+      File tmpFile = null;
+
+      verifyFilesExistance(fileOneShouldExist, fileTwoShouldExist);
+
+      if (fileThreeShouldExist)
+      {
+         tmpFile = new File(dirPath, FILENAMES[2]);
+         Assert.assertTrue("third file should exist", tmpFile.exists());
+      }
+      else
+      {
+         tmpFile = new File(dirPath, FILENAMES[2]);
+         Assert.assertFalse("third file should not exist", tmpFile.exists());
+      }
+   }
+
+   private void verifyCommitMsgs()
+   {
       commits = myProject.getFacet(UndoFacet.class).getStoredCommits();
       commitMsgs = extractCommitMsgs(commits);
-
-      verifyForgeProjectFileExists();
 
       Assert.assertEquals("wrong number of commits in the history branch",
                getHistoryBranchSize(), commitMsgs.size());
@@ -201,10 +225,18 @@ public class UndoFacetMultipleOpsTest extends AbstractShellTest
       }
    }
 
-   private void verifyForgeProjectFileExists()
+   private static List<String> extractCommitMsgs(final Iterable<RevCommit> collection)
    {
-      File forgeXml = new File(dir.getFullyQualifiedName() + "/src/main/resources/META-INF/forge.xml");
-      Assert.assertTrue("forge project file doesn't exist anymore", forgeXml.exists());
+      List<String> commitMsgs = new ArrayList<String>();
+
+      Iterator<RevCommit> iter = collection.iterator();
+      while (iter.hasNext())
+      {
+         RevCommit commit = iter.next();
+         commitMsgs.add(commit.getFullMessage());
+      }
+
+      return commitMsgs;
    }
 
    private int getHistoryBranchSize()
